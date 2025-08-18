e7338f1e4d68bc5a4710d972d96740d4e09d27aa
Marketa Solanikova <solanikova@promark.sk>
2025-08-18 21:14:46 +0200

    fix unit test

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,16 +4,16 @@ import pytest
 
 def test_lambda_handler_does_not_throw_exception_when_empty_event():
     try:
-        lambda_handler({})
+        lambda_handler({}, None)
     except Exception as e:
         pytest.fail("Unexpected MyError ..")
 
 def test_lambda_handler_does_not_throw_exception_when_parameter_with_correct_value():
     try:
-        lambda_handler({"parameter1": "correct value"})
+        lambda_handler({"parameter1": "correct value"}, None)
     except Exception as e:
         pytest.fail("Unexpected MyError ..")
 
 def test_lambda_handler_does_throw_exception_when_wrong_parameter_value_provided():
     with pytest.raises(Exception):
-        lambda_handler({"parameter1": "exc"})
+        lambda_handler({"parameter1": "exc"}, None)
