81abe5ea125889e061631bd69032ad15f9a30d74
Marketa Solanikova <solanikova@promark.sk>
2025-08-18 20:58:24 +0200

    do the code change in application to test the pipeline

--- src/data_processor/app.py
+++ src/data_processor/app.py
@@ -4,8 +4,12 @@ from data_processor.conf import config
 def lambda_handler(event):
     print(config.customer)
 
-    param = event.get("parameter1")
-    print(param)
+    param1 = event.get("parameter1")
+    print(param1)
+
+    param2 = event.get("parameter2")
+    print(param2)
+
 
     if event.get("parameter1") == "exc":
         raise Exception(f"parameter1 has wrong value: {event.get("parameter1")}")
